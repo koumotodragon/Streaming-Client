9bb437ec3173ad41b2d0b0c147ff653035ac1990
Bhoomi Kathiriya <bhoomi@Bhoomis-MacBook-Pro.local>
2024-02-02 14:48:38 -0500

    Category section UI created

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import "./Categories.css"
+import React from "react";
+import "./Categories.css";
+import { Pill } from "../UI";
 
 export const Categories = () => {
-    return (
-        <div className="Categories-Container">
-             I am a Category slider, Pill will go inside me!
-        </div>
-    )
-}
+  const categories = [
+    "News",
+    "Ratchet",
+    "Drake",
+    "Street Fight",
+    "Homeless",
+    "Jay Z",
+    "Kardashian",
+    "Kodak Black",
+    "GirlFight",
+    "Comedy SKit",
+    "Road Rage",
+    "Megan Thee Stallion",
+    "Lil Baby",
+    "XXXTentacion",
+  ];
+
+  return ( 
+    <div
+      className="Categories-Container"
+      style={{
+        overflowX: "scroll",
+        scrollBehavior: "smooth",
+      }}
+    >
+      {categories.map((item) => {
+        return <Pill item={item} />;
+      })}
+    </div>
+  );
+};
